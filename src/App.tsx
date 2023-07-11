740bc4c9ab31173cbc3ed635bd63aa2baf890dbb
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-07-11 14:53:19 +0300

    add paramInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,118 @@
-function App() {
-  return <></>;
+import { useEffect, useState } from "react";
+
+interface Param {
+  id: number;
+  name: string;
+  type: "string";
+}
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+interface ParamInputProps {
+  paramValue: ParamValue;
+  param: Param;
+  onChange: (id: number, value: string) => void;
+}
+
+function ParamInput({ paramValue, param, onChange }: ParamInputProps) {
+  const [value, setValue] = useState<string>(
+    paramValue?.value ? paramValue.value : ""
+  );
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
+  useEffect(() => {
+    onChange(param.id, value);
+  }, [value, param.id]);
+
+  return (
+    <div
+      style={{
+        width: "300px",
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "10px 0",
+      }}
+    >
+      <label
+        htmlFor={param.id.toString()}
+        style={{ width: "50%", textAlign: "center" }}
+      >
+        {param.name}
+      </label>
+      <input
+        id={param.id.toString()}
+        type="text"
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+function App({ params, model }: Props) {
+  const [modelState, setModelState] = useState<Model>(model);
+
+  function getModel(): Model {
+    return modelState;
+  }
+
+  function getParamValue(id: number): ParamValue | undefined {
+    return modelState.paramValues.find((v) => v.paramId === id);
+  }
+
+  function changeParamValue(id: number, newValue: string): void {
+    const newModel: Model = { ...modelState };
+    if (modelState.paramValues.find((elem) => elem.paramId === id)) {
+      newModel.paramValues = newModel.paramValues.map((paramValue) => {
+        if (paramValue.paramId === id) {
+          paramValue.value = newValue;
+        }
+        return paramValue;
+      });
+    } else {
+      newModel.paramValues.push({ paramId: id, value: newValue });
+    }
+
+    setModelState(newModel);
+  }
+
+  useEffect(() => {
+    console.log(getModel());
+  }, [modelState]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      {params.map((param) => {
+        return (
+          <ParamInput
+            key={param.id}
+            param={param}
+            paramValue={getParamValue(param.id) as ParamValue}
+            onChange={changeParamValue}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+const params = [
+  {
+    id: 1,
+    name: "Назначение",
+  },
+  {
+    id: 2,
+    name: "Длина",
+  },
+];
+
+const model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное",
+    },
+    {
+      paramId: 2,
+      value: "макси",
+    },
+  ],
+};
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <App params={params} model={model} />
   </React.StrictMode>
 );
